8fbc8ddf1931324d414986130e3e94a8376979be
Manoj <103386321+Manoj028@users.noreply.github.com>
2022-05-10 19:13:22 +0530

    Add files via upload

---
+++ project -1/app.js
@@ -0,0 +1,15 @@
+const colors=["Blue","red","green","orange","pink","White","Purple"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+    
+}
